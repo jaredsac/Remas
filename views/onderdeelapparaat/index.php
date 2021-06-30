b5b8b66e403a1ac2d91e9598789cb74682a1c130
Jared Sackey <jared.sackey@student.rocva.nl>
2021-06-30 13:38:47 +0200

    change

--- views/onderdeelapparaat/index.php
+++ views/onderdeelapparaat/index.php
@@ -18,7 +18,8 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Create Onderdeelapparaat', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); 
+    ?>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
@@ -27,8 +28,20 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\SerialColumn'],
 
             'id',
-            'apparaatID',
-            'onderdeelID',
+            [
+                'attribute' => 'apparaatID',
+                'label'     => 'Apparaat',
+                'value'     => 'apparaat.naam'
+                // hier verander ik  ik de naaam van apparaatID naar Appraat
+                //vranderd de naam van de apparaat inplaats van een nummer staat er een naam
+            ],
+            [
+                'attribute' => 'onderdeelID',
+                'label'     => 'Onderdeel',
+                'value'     => 'onderdeel.naam'
+                // hier verander ik  ik de naaam van onderdeelID naar onderdeel
+                //vranderd de naam van de onderdeel inplaats van een nummer staat er een naam
+            ],
             'percentage',
 
             ['class' => 'yii\grid\ActionColumn'],
