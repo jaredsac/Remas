5a2f61f34a4e4813b8b891cc44f693f6748a14e1
Jared Sackey <jared.sackey@student.rocva.nl>
2021-07-01 09:46:44 +0200

    kleine change

--- views/site/login.php
+++ views/site/login.php
@@ -34,14 +34,14 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <div class="form-group">
             <div class="col-lg-offset-1 col-lg-11">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                <?= Html::submitButton('Login', ['class' => 'btn btn-info', 'name' => 'login-button']) ?>
             </div>
         </div>
 
     <?php ActiveForm::end(); ?>
 
     <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
+        You may login with <strong>username is you name</strong> or <strong>paswoord can be your surname</strong>.<br>
         To modify the username/password, please check out the code <code>app\models\User::$users</code>.
     </div>
 </div>
